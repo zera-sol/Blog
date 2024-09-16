2dc4aa472245279dacf2d9d0606b01dfd095aa44
@zera-sol <zedomanwithjesu1994@gmail.com>
2024-09-16 01:35:27 -0700

    commit 8

--- backend/index.js
+++ backend/index.js
@@ -1,81 +1,10 @@
-require('dotenv').config();
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require("mongoose");
-var cors = require('cors');
-var port = process.env.PORT || '5000'
-//let's import the routes
+const express = require('express');
+const app = express();
 
-var usersRoute = require('./Routes/usersRoute');
-var postRoute = require('./Routes/postRoute')
-// database.js
-// Connect to MongoDB
-const uri = 'mongodb+srv://zedomanwithjesu1994:n0wBmb3UWKm5Bs7N@blog-db.qdksl.mongodb.net/?retryWrites=true&w=majority&appName=blog-db'
-//const localDB = "mongodb://127.0.0.1:27017/blogDB"
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})  
-.then(() => {console.log('Connected to MongoDB');
-}).catch((error) => {
-console.log('Connection error', error);
-});
-// Data base connection ended
-
-var app = express();
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-//app.use(cors()); this is used to connect the frontend and backend
-app.use(cors(
-  {
-    origin: 'https://zera-blog.vercel.app',
-    credentials: true
-  }
-));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use(express.json());
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-//let's use the routes
-app.use('/users', usersRoute);
-app.use('/posts', postRoute);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(5000, () => {
+  console.log('Server is running on port 3000');
 });
-
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port} And I am the backend`);
-});
-
-//module.exports = app;
-
-// zedomanwithjesu1994
-//n0wBmb3UWKm5Bs7N

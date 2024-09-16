0c1fbde487b69a1114091e3612f99b1ec27e12ad
@zera-sol <zedomanwithjesu1994@gmail.com>
2024-09-16 01:40:48 -0700

    commit 9

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const usersRoute = require('./Routes/usersRoute');
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+app.use('/users', usersRoute);
 
 app.listen(5000, () => {
   console.log('Server is running on port 3000');

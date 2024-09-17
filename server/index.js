fc9ad2da231b530029b6476ea8b50c6495750a90
@zera-sol <zedomanwithjesu1994@gmail.com>
2024-09-17 02:32:52 -0700

    commit

--- server/index.js
+++ server/index.js
@@ -1,13 +1,8 @@
-require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 5001;
+const port =  5001;
 
-// Middleware
-app.use(cors());
-app.use(express.json()); // For parsing JSON requests
 
 // Routes
 

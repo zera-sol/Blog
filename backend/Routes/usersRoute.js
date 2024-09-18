10722293da35c463b655bddbc032c5e5e925814b
@zera-sol <zedomanwithjesu1994@gmail.com>
2024-09-18 05:18:45 -0700

    commit 4

--- backend/Routes/usersRoute.js
+++ backend/Routes/usersRoute.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const userController = require('../Controllers/userController');
 const userModel = require('../Models/userModel');
 
+router.get('/', (req, res) => {
+    res.send('Welcome to the Users Api');
+});
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.get('/profile', userController.profile);

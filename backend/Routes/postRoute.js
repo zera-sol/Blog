038e26d22fbfd9fceb12c011e78f98df85084a4b
@zera-sol <zedomanwithjesu1994@gmail.com>
2024-09-20 14:44:48 -0700

    commit 13

--- backend/Routes/postRoute.js
+++ backend/Routes/postRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const upload = multer({ storage: multer.memoryStorage() }); // Use memory storage
+const uploadMiddleware = multer({ storage: multer.memoryStorage() }); // Use memory storage
 const router = express.Router();
 const postController = require("../Controllers/postController");
 

4f7b2ad6f3e40b923bb4eba50e405f6cc871e550
@zera-sol <zedomanwithjesu1994@gmail.com>
2024-09-20 11:39:18 -0700

    commit 1

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -6,7 +6,45 @@ const jwt = require('jsonwebtoken');
 const SECRET_KEY = process.env.SECRET_KEY;
 //register function to register a user
 const register = async (req, res) => {
-res.send('Welcome to the Users Register Api');
+const formData ={
+    fullName: 'Biruk Solomon',
+    email: 'bura@gmail.com',
+    password: "123456"
+}
+const { email } = formData;
+//now we will try to save the user to the database
+ try {
+        //we need to check whether the user already exists
+        const Users = await User.find({ email});
+
+        if (Users.length > 0) {
+            return res.status(400).json({
+                message: 'User already exists'
+            });
+        }
+        else{
+            //let's hash the password
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(formData.password, salt);
+            //let's create a us
+            //if the user does not exist, we will save the user to the database
+                const newUser = new User({
+                    name: formData.fullName,
+                    email: formData.email,
+                    password: hashedPassword,
+                });
+                await newUser.save();
+                res.status(201).json({
+                    message: 'User registered successfully',
+                    data: newUser
+                });
+            }
+    } catch (error) {
+            res.status(500).json({
+                message: 'An error occurred',
+                error: error.message
+            });
+    }
 }
 //Login function to login the user 
 const login = async (req, res) => {
